ebdab2f51339748569527fc443753de7fc05d425
000407 <senad.407@gmail.com>
2019-09-15 00:39:26 +0900

    Initial commit

---
+++ FileExtensionManager.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FileExtApp
+{
+    partial class FileExtensionManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileType = new System.Windows.Forms.TextBox();
+            this.btnAddExt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtExtension = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.twFileTypes = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnListAll = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFileType
+            // 
+            this.txtFileType.Location = new System.Drawing.Point(382, 23);
+            this.txtFileType.Name = "txtFileType";
+            this.txtFileType.Size = new System.Drawing.Size(191, 20);
+            this.txtFileType.TabIndex = 1;
+            // 
+            // btnAddExt
+            // 
+            this.btnAddExt.Location = new System.Drawing.Point(383, 98);
+            this.btnAddExt.Name = "btnAddExt";
+            this.btnAddExt.Size = new System.Drawing.Size(112, 23);
+            this.btnAddExt.TabIndex = 3;
+            this.btnAddExt.Text = "Add Extension";
+            this.btnAddExt.UseVisualStyleBackColor = true;
+            this.btnAddExt.Click += new System.EventHandler(this.BtnAddExt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Extension";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Program Name";
+            // 
+            // txtExtension
+            // 
+            this.txtExtension.Location = new System.Drawing.Point(382, 61);
+            this.txtExtension.Name = "txtExtension";
+            this.txtExtension.Size = new System.Drawing.Size(191, 20);
+            this.txtExtension.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(296, 204);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete Selected";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // twFileTypes
+            // 
+            this.twFileTypes.FormattingEnabled = true;
+            this.twFileTypes.Location = new System.Drawing.Point(30, 21);
+            this.twFileTypes.Name = "twFileTypes";
+            this.twFileTypes.Size = new System.Drawing.Size(240, 264);
+            this.twFileTypes.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(296, 262);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnListAll
+            // 
+            this.btnListAll.Location = new System.Drawing.Point(296, 233);
+            this.btnListAll.Name = "btnListAll";
+            this.btnListAll.Size = new System.Drawing.Size(112, 23);
+            this.btnListAll.TabIndex = 7;
+            this.btnListAll.Text = "List All";
+            this.btnListAll.UseVisualStyleBackColor = true;
+            this.btnListAll.Click += new System.EventHandler(this.BtnListAll_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(461, 151);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(112, 23);
+            this.btnFind.TabIndex = 5;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.BtnFind_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(296, 153);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(146, 20);
+            this.txtFind.TabIndex = 4;
+            // 
+            // FileExtensionManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 319);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnListAll);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.twFileTypes);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtExtension);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddExt);
+            this.Controls.Add(this.txtFileType);
+            this.Name = "FileExtensionManager";
+            this.Text = "File Extension Manager";
+            this.Load += new System.EventHandler(this.FileExtensionManager_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFileType;
+        private System.Windows.Forms.Button btnAddExt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtExtension;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox twFileTypes;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnListAll;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtFind;
+    }
+}
+
---
+++ FileExtensionManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FileExtApp
+{
+    /// <summary> 
+    /// Form stores and displays file extensions and default program
+    /// using a key/value generic collection class
+    /// </summary>
+    public partial class FileExtensionManager : Form
+    {
+        //TO DO 
+        // Declare an appropriate instance variable here to store the entries as a
+        // key (file extension) value (default program) pair
+        public FileExtensionManager()
+        {
+            
+        }
+
+        private void BtnAddExt_Click(object sender, EventArgs e)
+        {
+            //TO DO add code to create new entry
+            //should show appropriate error if unable to create entry
+            //set textDisplay to new entry
+            //clear other text boxes
+            //should update data file by calling UpdateDataFile if successful 
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            //TO DO add code to delete current entry
+            //using extenstion in textExtension text box
+            //should show appropriate error messages if cannot delete
+            //should update datafile by calling UpdateDataFile if delete is successful
+            //should clear all textBoxes 
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to delete all entries ?", "Confirm delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                //TO DO – add code to clear all entries in collection  
+                //remember to also call UpdateDataFile         
+            }
+            //TO DO add code to clear all text boxes
+        }
+
+            private void BtnListAll_Click(object sender, EventArgs e)
+        {
+            //TO DO should clear textDisplay and then
+            //should iterate through collection object
+            //and append text to textDisplay text box 
+        }
+
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            //TO DO add code to find entry
+            // should update textDisplay appropriately if found 
+        }
+
+        /// <summary> 
+        /// helper method for writing data to data file 
+        /// </summary> 
+        private void UpdateDataFile() {
+            //TO DO
+            //should write collection to file datafile.dat
+            //easiest way is to use System.Runtime.Serialization libraries
+        } 
+
+        /// <summary>
+        /// helper method for reading data from data file
+        /// </summary>
+        private void ReadDataFile() {
+            //TO DO
+            //should read in collection from file datafile.dat if it exists
+            //easiest way is to use System.Runtime.Serialization libraries
+        } 
+
+        /// <summary> 
+        /// Event handler method called when for load
+        /// should read entries from data file (if it exists) 
+        /// or create a new collection object (the instance variable) 
+        /// </summary> 
+        private void FileExtensionManager_Load(object sender, EventArgs e)
+        {
+            //TO DO
+            // create new Collection object to store entries
+            // and if data file exist, read in the data
+            // by calling ReadDataFile method 
+        }
+    }
+}
